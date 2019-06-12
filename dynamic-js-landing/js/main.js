8a4848055411b2a873b02e34cb6ec1110b59d71e
unknown <devethancraig@gmail.com>
2019-06-11 21:28:44 -0600

    Finished

--- dynamic-js-landing/js/main.js
+++ dynamic-js-landing/js/main.js
@@ -28,23 +28,72 @@ function addZero(num) {
 //Set Background
 function setBgGreet() {
     let today = new Date(),
-      hour = today.getHours();
-  
+        hour = today.getHours();
+
     if (hour < 12) {
-      // Morning
-      document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-      greeting.textContent = 'Good Morning, ';
+        // Morning
+        document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+        greeting.textContent = 'Good Morning, ';
     } else if (hour < 18) {
-      // Afternoon
-      document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-      greeting.textContent = 'Good Afternoon, ';
+        // Afternoon
+        document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+        greeting.textContent = 'Good Afternoon, ';
     } else {
-      // Evening
-      document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
-      greeting.textContent = 'Good Evening, ';
-      document.body.style.color = 'white';
+        // Evening
+        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        greeting.textContent = 'Good Evening, ';
+        document.body.style.color = 'white';
     }
-  }
+}
+
+//Set Name
+function setName(e) {
+    if (e.type === 'keypress') {
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
+}
+
+//Set Focus
+function setFocus(e) {
+    if (e.type === 'keypress') {
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+//Get Name
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+//Get Focus
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 
 showTime();
 setBgGreet();
+getName();
+getFocus();
